9269fa0d5771b4e256c55dcdb11c2f913c84f2a2
gusstavo01 <gustavo14parenteagc@gmail.com>
2024-01-01 20:26:06 -0300

    api github dio

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Providers from './providers';
+ 
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Providers />
+  </React.StrictMode>
+);
+
+ 
---
+++ src/providers.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import GithubProvider from "./providers/github-provider";
+import { ResetCSS } from "./global/resetCSS";
+import App from './App';
+
+
+function Providers() {
+  return (
+    <main>
+      <GithubProvider>
+        <ResetCSS/>
+        <App/>
+      </GithubProvider>
+    </main>
+  )
+}
+
+export default Providers;
